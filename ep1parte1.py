f5ff7b2d5c8b1c534892768746be5d84f82746c3
Thalles Carneiro <thallescarneiro@usp.br>
2021-05-18 11:22:59 -0300

    added fancy plotting things and now the integrator works

--- ep1parte1.py
+++ ep1parte1.py
@@ -2,6 +2,7 @@ import math
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
+
 g = 9.8 #m/s^2
 m_total = 1939 #kg
 L = 2.95 #m
@@ -34,7 +35,7 @@ def edo(condicoes):
     theta1 = condicoes[2]
     theta2 = condicoes[3]
 
-    theta2_d = (((theta2*m1*L)**2)*math.sin(theta1)*math.cos(theta1) - 2*I*w*theta2*m_total + m1*L*math.sin(theta1)*(F-F1*math.cos(theta1)))/(m1*(L**2)*(m_total-m1*(math.sin(theta1)**2)))
+    theta2_d = (-2*I*w*(theta2**2)*m_total +m1*L*math.sin(theta1)*(F -F1*math.cos(theta1) +m1*L*(theta2**2)*math.cos(theta1)))/(m1*(L**2)*(m_total-m1*(math.sin(theta1)**2)))
 
     x2_d = (-F + F1*math.cos(theta1) -m1*L*(math.cos(theta1)*(theta2**2) +theta2_d*math.sin(theta1)))/(m_total)
 
@@ -106,14 +107,21 @@ def rk4(c_i,t,dt):
 
 def graficos(solucao,aceleracoes,t,dt):
     fig, axis = plt.subplots(2,1)
-    axis[0].plot(np.arange(0.0,t,dt),solucao[:,1])
-    axis[0].plot(np.arange(0.0,t,dt),aceleracoes[:,0])
+    vel_lin,= axis[0].plot(np.arange(0.0,t,dt),solucao[:,1])
+    ac_lin, =axis[0].plot(np.arange(0.0,t,dt),aceleracoes[:,0])
+    axis[0].set(xlabel='tempo (s)',title=r' $\.x$ e $\ddotx$ com passo h =' + str(dt))
+    axis[0].legend((vel_lin,ac_lin),(" $\.x$(m/s)"," $\ddotx$(m/s²)"))
+    axis[0].grid()
 
-    axis[1].plot(np.arange(0.0,t,dt),solucao[:,3])
-    axis[1].plot(np.arange(0.0,t,dt),aceleracoes[:,1])
+
+    pos_ang,=axis[1].plot(np.arange(0.0,t,dt),solucao[:,2]*(180/math.pi))
+    vel_ang = axis[1].plot(np.arange(0.0,t,dt),solucao[:,3]*(180/math.pi))
+    axis[1].set(xlabel='tempo (s)',title=r' $\theta$ e $\.\theta$ com passo h =' + str(dt))
+    axis[1].legend((vel_lin,ac_lin),('$\\theta$ (rad)','$\dot \\theta$ (rad/s)'))
+    axis[1].grid()
     
     plt.show()
 
 
-solucao,aceleracoes =rk4(condicoes_iniciais,20,0.01)
+solucao,aceleracoes =rk2(condicoes_iniciais,20,0.01)
 graficos(solucao,aceleracoes,20,0.01)
